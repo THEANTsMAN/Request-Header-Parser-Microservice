4f82a6bd68a8901fb57b969491340b40ccdaa7cf
Anthony Lefebvre-Allen <th3antsman@gmail.com>
2018-04-19 13:20:22 +1200

    Sorted out handling the local machine vs external server regarding ip address pulled from the header. Also updated the README to note this.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Request Header Parser Microservice
-**********************
+-----------------------
 
 Runs an micro service that parses the request header and returns a json object showing the following information:
 -  IP address in the header that the request came from,
 -  Language in the request header
 -  The user agents software platform in the header.
 
-**********************
+-----------------------
 
 This is built to be used with glitch and an example of the app can be found [Here](https://charming-screen.glitch.me/)
 
-______________________
+-----------------------
 
 ### Running the App
 
@@ -36,3 +36,10 @@ PORT=XXXX node server.js
 ```
 
 Where __XXXX__ is the port number you want it to run on.
+
+-----------------------
+
+#### Notes
+
+This microservice was built to run on both glitch.com and on a local machine, so your mileage may vary depending on how you use the microservice.
+The main difference between running on glitch (i.e an external server) vs a localhost (i.e your personal machine) is the way the IP address is handled and found in the header. For the local machine there is likely not an x-forwarded-for key, so instead the host key is used, and visa versa for running one glitch or other external server.
--- server.js
+++ server.js
@@ -9,18 +9,27 @@
 var express = require('express');
 var app = express();
 
-app.get("*", function (request, response) {
-  let jsonHeader = JSON.parse(JSON.stringify(request.headers));
-  console.log(jsonHeader);
-  let resJSON = {
-    'ipaddress': jsonHeader["x-forwarded-for"].split(',')[0],
-    'language': jsonHeader["accept-language"].split(',')[0],
-    'software': jsonHeader["user-agent"].split("(").toString().split(")")[0].split(',')[1]
-  };
-  response.send(resJSON);
+app.get("*", function(request, response) {
+    let resJSON = {};
+    let jsonHeader = JSON.parse(JSON.stringify(request.headers));
+    console.log(jsonHeader);
+
+    resJSON = {
+        'ipaddress': null,
+        'language': jsonHeader["accept-language"].split(',')[0],
+        'software': jsonHeader["user-agent"].split("(").toString().split(")")[0].split(',')[1]
+    };
+
+    if (jsonHeader.hasOwnProperty('x-forwarded-for')) {
+        resJSON.ipaddress = jsonHeader["x-forwarded-for"].split(',')[0];
+    } else if (jsonHeader.hasOwnProperty('host')) {
+        resJSON.ipaddress = jsonHeader["host"].split(',')[0];
+    }
+
+    response.send(resJSON);
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Microservice is listening on port ' + listener.address().port);
+var listener = app.listen(process.env.PORT, function() {
+    console.log('Microservice is listening on port ' + listener.address().port);
 });
